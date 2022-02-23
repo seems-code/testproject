05eafd0a1e8fb5ba9b732b59f0dbb539513a3286
seems-code <seemalodhi256@gmail.com>
2022-02-23 11:12:53 +0530

    new Test4 and changes in Test2

---
+++ TestProject/src/Test4.java
@@ -0,0 +1,7 @@
+
+public class Test4 {
+	public static void main(String[] args) {
+		System.out.println("new class for checking git process");
+		System.out.println("will check here to resolve the conflicts");
+	}
+}
